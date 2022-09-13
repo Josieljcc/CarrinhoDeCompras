87fe1baec22f7448dff2f2c4b9dcbee6727e7567
Josiel da Costa <josiel.jcc@hotmail.com>
2022-09-13 14:12:29 -0300

    shoppinCart

---
+++ repo-filters-by-project/sd-0x-project-stranger-things/trybe-filter-repo.sh
@@ -0,0 +1,13 @@
+### GIT FILTER-REPO ###
+
+## NÃO EXECUTE ESSE SCRIPT DIRETAMENTE
+## Esse script foi feito para uso do
+## script 'publisher.sh' fornecido
+## pela Trybe.
+
+[[ $# == 1 ]] && \
+[[ $1 == "trybe-security-parameter" ]] && \
+git filter-repo \
+    --path assets \
+    --path README.md \
+    --invert-paths --force
